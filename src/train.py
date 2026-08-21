c7784388304bc313c3c5abdd0c33e5f418b73905
Hemant Malviya <editmyphoto59@gmail.com>
2026-08-21 16:16:46 +0530

    Add initial training script

---
+++ src/train.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("data\Housing.csv")
+def train():
+    print("data loading\n")
+    print("..........Training model..........\n")
+
+
+    print("..........Training completed.......... :)")
+
+if __name__ == "__main__" :
+    train()
